661cc3893541e41fe88c0f009567d1b83c414510
srrae <joel.serrano@estudiantat.upc.edu>
2026-03-24 19:28:42 +0100

    Apartat 2 - Rellotge RTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,49 @@
 #include <Arduino.h>
 
-void anotherTask(void * parameter);
+const int led = 4;
+SemaphoreHandle_t sem;
 
-void setup()
-{
+void tOn(void *p);
+void tOff(void *p);
+
+void setup() {
   Serial.begin(115200);
-  xTaskCreate(
-    anotherTask,
-    "another Task", 
-    10000,
-    NULL,
-    1, 
-    NULL
-  );
+  pinMode(led, OUTPUT);
+
+  Serial.println("--- SISTEMA INICIAT: COMPROVANT LED PIN 4 ---");
+  digitalWrite(led, HIGH); delay(500);
+  digitalWrite(led, LOW); delay(500);
+  Serial.println("--- COMPROVACIÓ OK: INICIANT TASQUES ---");
+
+  sem = xSemaphoreCreateBinary();
+  xSemaphoreGive(sem);
+
+  xTaskCreate(tOn, "ON", 2048, NULL, 1, NULL);
+  xTaskCreate(tOff, "OFF", 2048, NULL, 1, NULL);
 }
 
-void loop() {
-  Serial.println("this is ESP32 Task");
-  delay(1000);
-}
+void loop() {}
 
-void anotherTask(void * parameter) {
-  for(;;) {
-    Serial.println("this is another Task");
-    delay(1000);
+void tOn(void *p) {
+  for (;;) {
+    if (xSemaphoreTake(sem, portMAX_DELAY) == pdTRUE) {
+      digitalWrite(led, HIGH);
+      Serial.println("LED: ON");
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      xSemaphoreGive(sem);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
   }
-  vTaskDelete(NULL);
+}
+
+void tOff(void *p) {
+  for (;;) {
+    if (xSemaphoreTake(sem, portMAX_DELAY) == pdTRUE) {
+      digitalWrite(led, LOW);
+      Serial.println("LED: OFF");
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      xSemaphoreGive(sem);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+  }
 }
